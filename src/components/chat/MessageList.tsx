457c447435b49bb51b0dc8dcabc913ee2fb3203f
Uday Raj <udayraj.vadeghar@gmail.com>
2024-08-26 12:49:35 +0530

    Created MessageList

--- src/components/chat/MessageList.tsx
+++ src/components/chat/MessageList.tsx
@@ -1,5 +1,66 @@
+import { messages, USERS } from "@/db/dummy";
+import { cn } from "@/lib/utils";
+import { AnimatePresence, motion } from "framer-motion";
+import { Avatar, AvatarImage } from "../ui/avatar";
+
 const MessageList = () => {
-  return <div>hi</div>;
+  const currentUser = USERS[0];
+  const selectedUser = USERS[1];
+  return (
+    <div className="w-full overflow-y-auto overflow-x-hidden h-full flex flex-col">
+      <AnimatePresence>
+        {messages.map((message, index) => (
+          <motion.div
+            key={index}
+            layout
+            initial={{ opacity: 0, scale: 1, y: 50, x: 0 }}
+            animate={{ opacity: 1, scale: 1, y: 0, x: 0 }}
+            exit={{ opacity: 0, scale: 1, y: 1, x: 0 }}
+            transition={{
+              opacity: { duration: 0.1 },
+              layout: { type: "spring", bounce: 0.3 },
+              duration: messages.indexOf(message) * 0.05 + 0.2,
+            }}
+            style={{ originX: 0.5, originY: 0.5 }}
+            className={cn(
+              "flex flex-col gap-2 p-4 whitespace-pre-wrap",
+              message.senderId === currentUser.id ? "items-end" : "items-start"
+            )}
+          >
+            <div className="flex gap-3 items-center">
+              {message.senderId === selectedUser.id && (
+                <Avatar className="flex justify-center items-center">
+                  <AvatarImage
+                    src={selectedUser.image}
+                    alt="User Image"
+                    className="border-2 border-white rounded-full"
+                  />
+                </Avatar>
+              )}
+              {message.messageType === "text" ? (
+                <span className="bg-accent p-3 rounded-md max-w-xs"></span>
+              ) : (
+                <img
+                  src={message.content}
+                  alt="Message Image "
+                  className="border p-2 rounded h-40 md:h-52 object-cover"
+                ></img>
+              )}
+              {message.senderId === currentUser.id && (
+                <Avatar className="flex justify-center items-center">
+                  <AvatarImage
+                    src={currentUser.image}
+                    alt="User Image"
+                    className="border-2 border-white rounded-full"
+                  />
+                </Avatar>
+              )}
+            </div>
+          </motion.div>
+        ))}
+      </AnimatePresence>
+    </div>
+  );
 };
 
 export default MessageList;
