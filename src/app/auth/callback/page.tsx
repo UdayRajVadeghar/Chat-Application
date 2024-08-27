f745c6d41f2cf74c0614dcbf3dde998ddf5081f3
Uday Raj <udayraj.vadeghar@gmail.com>
2024-08-27 19:20:59 +0530

    Redis db set

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,5 +1,15 @@
+import { Loader } from "lucide-react";
+
 const page = () => {
-  return <div>Auth callback page</div>;
+  return (
+    <div className="mt-20 w-full flex justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <Loader className="w-10 h-10 animate-spin text-muted-foreground" />
+        <h3 className="text-xl font-bold">redirecting...</h3>
+        <p>Please wait</p>
+      </div>
+    </div>
+  );
 };
 
 export default page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import PreferencesTab from "@/components/PreferencesTab";
 import ChatLayout from "@/components/chat/ChatLayout";
+import { redis } from "@/lib/db";
 import { cookies } from "next/headers";
 
-export default function Home() {
+export default async function Home() {
   const layout = cookies().get("react-resizable-panels:layout");
   const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
+
+  await redis.set("foo", "bar");
+
+  const data = await redis.get("foo");
+
+  console.log(data);
 
   return (
     <main className="flex h-screen flex-col  items-center justify-center pd-4 md:px-24 py-32 gap-4">
